46360deb0297e57c6dbf8b021b8e4b961be405a6
Eugen101 <55134543+EugenKostro@users.noreply.github.com>
2020-03-24 11:54:13 +0100

    Add files via upload

---
+++ Nasljeđivanje/Nasljeđivanje/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nasljeđivanje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ucenik u1 = new Ucenik();
+            u1.Ime = "Marko";
+            u1.Prezime = "Marulić";
+            u1.Razred1 = 3;
+            Nastavnik n1 = new Nastavnik();
+            n1.Ime = "Luciano";
+            n1.Prezime = "Pavarotti";
+            n1.Predmet1 = "Programiranje";
+
+            Console.WriteLine(u1.ToString());
+
+            Console.WriteLine(n1.DohvatiPodatke());
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Nasljeđivanje/Nasljeđivanje/Ucenik.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nasljeđivanje
+{
+    class Ucenik : Osoba
+    {
+        int Razred;
+
+        public int Razred1 { get => Razred; set => Razred = value; }
+    }
+}
